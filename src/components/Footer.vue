ae3556f6dcac86f07405b6e38fd19ad52632e383
Rumble <109781840+BiliRumble@users.noreply.github.com>
2023-08-14 20:37:01 +0000

    try: retry

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -15,9 +15,10 @@ onMounted(async () => {
   try {
     const response = await axios.get(`https://api.github.com/repos/BiliRumble/home/commits`);
 
-    const fullSha = response.data.commit.sha;
+    const fullSha = response.data.sha;
     commitSha.value = fullSha.substring(0, 7);
   } catch (error) {
+    commitSha.value = "获取出现错误。"
     console.error("[error] Failed to get GithubAPI: " + error);
   }
 });
