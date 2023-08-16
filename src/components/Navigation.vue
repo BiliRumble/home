f2fa7269a48cafb1171ee5f1e5581ca8e086c8aa
Rumble <109781840+BiliRumble@users.noreply.github.com>
2023-08-16 14:32:52 +0000

    feat: update style.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -61,14 +61,6 @@ nav {
     margin-top: 10px;
     flex-wrap: wrap;
     color: #f4f4f4;
-    transition: all 0.3s;
-
-    box-shadow: 0px 5px 12px rgba(0,0,0,0.5);
-}
-
-.navigation:hover {
-    transform: translateY(-2px) scale(1.01,1.01);
-    box-shadow: 0px 10px 20px rgba(0,0,0,0.3);
 }
 
 .navigation__item {
@@ -104,6 +96,13 @@ i {
     font-style: normal;
     font-size: 18px;
     color: rgb(255,255,255);
+    transition: all 0.3s;
+    box-shadow: 0px 5px 12px rgba(0,0,0,0.5);
+}
+
+.i:hover {
+    transform: translateY(-2px) scale(1.01,1.01);
+    box-shadow: 0px 10px 20px rgba(0,0,0,0.3);
 }
 
 .social {
