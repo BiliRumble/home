f91651d7e933a8af6b63032bb8bc1609c380fa1e
Rumble <109781840+BiliRumble@users.noreply.github.com>
2023-09-10 05:00:02 +0000

    chore: 日常更新

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import './assets/global.scss'
+import '@/assets/global.scss'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 
 createApp(App)
 .mount('#app')
