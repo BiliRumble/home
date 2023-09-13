45053c0a3551077a7e15c51cb6d314ea0b9bc576
Rumble <109781840+BiliRumble@users.noreply.github.com>
2023-09-13 04:20:00 +0000

    feat: 详细拆分 Header 部分

---
+++ src/components/Desc.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+      <h1 class="name">{{ name }}</h1>
+      <p class="description">{{ des }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+  
+  const name = ref(import.meta.env.VITE_DESC_NAME);
+  const des = ref(import.meta.env.VITE_DESC_DES);
+</script>
+
+<style scoped lang="scss">
+  /* Desc样式 */
+  .name {
+    font-size: 2.5em;
+    text-align: center;
+    color: var(--main-text-color);
+    transition: all 1.00s ease;
+  }
+  
+  .description {
+    font-size: 1em;
+    text-align: center;
+    color: var(--main-text-color);
+    transition: all 1.00s ease;
+  }
+  
+  /* 自适应 */
+  @media all and (max-width: 1100px) {
+    .name {
+      font-size: 2em;
+    }
+
+    .description {
+      font-size: 1em;
+    }
+  }
+  
+  @media all and (max-width: 340px) {
+    .name {
+      margin-bottom: 0.1em;
+      font-size: 1.5em;
+   }
+
+    .description {
+      font-size: 0.9em;
+    }
+  }
+</style>
